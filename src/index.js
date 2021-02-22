ee75a6636f7e0d3963b2d6282c582a349cb5bd6b
ssongseulki <ssongseulki@naver.com>
2021-02-22 15:16:32 +0900

    modify

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Game from './components/Game';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
